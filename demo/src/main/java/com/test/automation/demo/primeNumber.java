7112eb730f68a5eafc416f4501d34cc4b5a970a5
Nagarjuna Yalamanchili <nagarjuna.y99@gmail.com>
2020-01-07 18:38:12 +0530

    change

--- demo/src/main/java/com/test/automation/demo/primeNumber.java
+++ demo/src/main/java/com/test/automation/demo/primeNumber.java
@@ -10,20 +10,22 @@ package com.test.automation.demo;
 public class primeNumber {
 
 	public static boolean isPrime(int num) {
-		
-		for(int i=2; i*i<=num; i++) {
-			if(i%num == 0) {
+		if(num==1) {
+			return true;
+			
+		}
+		else
+		for(int i=2; i<=num/2; i++) {
+			if(num%i==0) {
 				return true;
 			}
 		}
 		return false;
+		
 	}
-	
-	
 	
 	
 	public static void main(String args[]) {
-		System.out.println(isPrime(5));
+		System.out.println(isPrime(4));
 	}
-	
 }
